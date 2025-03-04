92bc1d97dc84ba93d3d3a4240e766ba613a79c8a
李银超 <2429162154@qq.com>
2025-03-04 11:15:59 +0800

    [perf]优化Scrollbar样式

--- src/views/test.vue
+++ src/views/test.vue
@@ -12,37 +12,25 @@
       </div>
     </a-scrollbar>
 
+    <a-button @click="total += 10">点击total+10</a-button>
+    <a-button @click="total -= 10">点击total-10</a-button>
     <yc-scrollbar
-      style="height: 200px; width: 200px; overflow: auto"
+      style="height: 200px; width: 100%; overflow: auto"
       ref="scrollbarRef"
     >
       <div
-        style="height: 4000px; width: 4000px; background-color: aqua"
-        ref="contentRef"
+        style="min-height: 200px; background-color: aquamarine; width: 200px"
       >
-        <div v-for="i in 200" :key="i">{{ i }}</div>
+        <div v-for="i in total" :key="i">{{ i }}</div>
       </div>
     </yc-scrollbar>
-
-    <div>
-      <yc-tooltip trigger="click" position="bottom">
-        <a-button>测试</a-button>
-        <template #content>
-          <div class="popover-content">
-            <a-empty />
-          </div>
-        </template>
-      </yc-tooltip>
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, watch, watchEffect } from 'vue';
 import YcScrollbar from '@/packages/Scrollbar';
-// const visible = ref<boolean>(false);
-
-// const scrollbarRef = ref<InstanceType<typeof YcScrollbar>>();
+const total = ref(1);
 </script>
 
 <style lang="less" scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -49,13 +49,6 @@ export default defineConfig(({ mode }) => {
     server: {
       port,
       open: true,
-      // proxy: {
-      //   '/api': {
-      //     target: 'http://localhost:8080',
-      //     changeOrigin: true,
-      //     rewrite: (path) => path.replace(/^\/api/, ''),
-      //   },
-      // },
     },
     build: {
       outDir,
