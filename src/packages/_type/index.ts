27773d7e37c7c95af215cc0fe1cd75089a725b04
李银超 <2429162154@qq.com>
2025-03-05 11:01:23 +0800

    [feat]新增全局函数类型

--- src/packages/_type/index.ts
+++ src/packages/_type/index.ts
@@ -10,13 +10,11 @@ export type CloseType =
   | 'esc'
   | '';
 
-<<<<<<< HEAD
-export type Fn = (...args: any) => any;
-=======
 export type RenderContent = string | RenderFunction;
 
 export type ClassName =
   | string
   | Record<string, boolean>
   | (string | Record<string, boolean>)[];
->>>>>>> f7e68564cc962b7a25df852515cc9ed0ed39c386
+
+export type Fn = (...args: any) => any;
