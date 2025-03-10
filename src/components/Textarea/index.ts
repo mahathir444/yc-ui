4b64d67d9a69f772b4c8ccaeef087e92a27fd85f
李银超 <2429162154@qq.com>
2025-03-10 13:57:04 +0800

    [feat]完成textarea组件

---
+++ src/components/Textarea/index.ts
@@ -0,0 +1,14 @@
+import { App } from 'vue';
+import _Textarea from './index.vue';
+import { getComponentPrefix } from '@/components/_utils/global-config';
+
+export type TextareaInstance = InstanceType<typeof _Textarea>;
+export type { TextareaProps } from './type';
+
+const Textarea = Object.assign(_Textarea, {
+  install: (app: App) => {
+    app.component(getComponentPrefix() + _Textarea.name, _Textarea);
+  },
+});
+
+export default Textarea;
