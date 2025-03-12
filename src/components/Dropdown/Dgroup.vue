9e9d2a7455b0d9cd4e86f43bd457e3906c8e6926
李银超 <2429162154@qq.com>
2025-03-12 09:48:52 +0800

    [feat]新增dgroup

---
+++ src/components/Dropdown/Dgroup.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="yc-dropdown-group-title text-ellipsis">
+    <slot v-if="$slots.title" name="title" />
+    <template v-else>
+      {{ title }}
+    </template>
+  </div>
+  <slot />
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+  name: 'Dgroup',
+});
+defineProps<{
+  title?: string;
+}>();
+</script>
+
+<style lang="less" scoped>
+@import './style/dgroup.less';
+</style>
