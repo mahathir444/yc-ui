eb237b5094c04cf7eda5965b4b656b9342f459ae
李银超 <2429162154@qq.com>
2025-07-07 16:14:21 +0800

    [fix]修复card在grid布局下的padding样式不生效的问题

--- src/views/dev.vue
+++ src/views/dev.vue
@@ -4,36 +4,6 @@
 
 <script lang="ts" setup>
 import { ref, computed } from 'vue';
-const COLORS = [
-  'red',
-  'orangered',
-  'orange',
-  'gold',
-  'lime',
-  'green',
-  'cyan',
-  'arcoblue',
-  'purple',
-  'pinkpurple',
-  'magenta',
-  'gray',
-];
-
-const COLORS_CUSTOM = [
-  '#F53F3F',
-  '#7816FF',
-  '#00B42A',
-  '#165DFF',
-  '#FF7D00',
-  '#EB0AA4',
-  '#7BC616',
-  '#86909C',
-  '#B71DE8',
-  '#0FC6C2',
-  '#FFB400',
-  '#168CFF',
-  '#FF5722',
-];
 </script>
 
 <style lang="less" scoped>
@@ -43,5 +13,14 @@ const COLORS_CUSTOM = [
   display: flex;
   flex-direction: column;
   gap: 10px;
+}
+
+.card-demo {
+  width: 360px;
+  margin-left: 24px;
+  transition-property: all;
+}
+.card-demo:hover {
+  transform: translateY(-4px);
 }
 </style>
